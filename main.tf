f26668a7e1d5163a9d48e055253ed41f181da8f3
ashwinirajn <ashwinioffice25@gmail.com>
2026-08-11 12:13:44 +0300

    added new file and delted stale ones, created vnets n subnets

--- main.tf
+++ main.tf
@@ -18,3 +18,31 @@ resource "azurerm_resource_group" "rg" {
     test = "manual"
   }
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "vnet-lab"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space    = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "snet0" {
+  name = "Web"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name = azurerm_resource_group.rg.name
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "snet1" {
+  name = "App"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name = azurerm_resource_group.rg.name
+  address_prefixes = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "snet2" {
+  name = "DB"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name = azurerm_resource_group.rg.name
+  address_prefixes = ["10.0.3.0/24"]
+}
