41c8a24c770210a08c0522ce766690030022ae81
ashwinirajn <ashwinioffice25@gmail.com>
2026-08-13 16:40:48 +0300

    Added NSG rules

--- main.tf
+++ main.tf
@@ -20,29 +20,101 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name = "vnet-lab"
-  location = azurerm_resource_group.rg.location
+  name                = "vnet-lab"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space    = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "snet0" {
-  name = "Web"
+  name                 = "Web"
   virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name = azurerm_resource_group.rg.name
-  address_prefixes = ["10.0.1.0/24"]
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "snet1" {
-  name = "App"
+  name                 = "App"
   virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name = azurerm_resource_group.rg.name
-  address_prefixes = ["10.0.2.0/24"]
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_subnet" "snet2" {
-  name = "DB"
+  name                 = "DB"
   virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+############### NSG rules #################################
+resource "azurerm_network_security_group" "nsg0" {
+  name                = "nsgweb"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_prefixes = ["10.0.3.0/24"]
+
+  security_rule {
+    name                       = "allow-internet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "10.0.1.0/24"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "nsgapp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-web-snet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "10.0.1.0/24"
+    destination_address_prefix = "10.0.2.0/24"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg2" {
+  name                = "nsgdb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-app-snet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "10.0.2.0/24"
+    destination_address_prefix = "10.0.3.0/24"
+  }
+}
+
+########### NSG Association ######################
+
+resource "azurerm_subnet_network_security_group_association" "web_assoc" {
+  subnet_id                 = azurerm_subnet.snet0.id
+  network_security_group_id = azurerm_network_security_group.nsg0.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "app_assoc" {
+  subnet_id                 = azurerm_subnet.snet1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "db_assoc" {
+  subnet_id                 = azurerm_subnet.snet2.id
+  network_security_group_id = azurerm_network_security_group.nsg2.id
 }
